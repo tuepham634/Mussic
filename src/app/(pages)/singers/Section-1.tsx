934ca8d6beb429b786d2ffe70e190026405dcf7a
tuepham634 <152422581+tuepham634@users.noreply.github.com>
2025-03-21 17:05:36 +0700

    fix lần thứ n+1

--- src/app/(pages)/singers/Section-1.tsx
+++ src/app/(pages)/singers/Section-1.tsx
@@ -16,7 +16,7 @@ export default function Section1() {
       const data = snapshot.val();
       if (data) {
         //lặp qua mảng singerId xong tìm bản ghi ca sĩ có id đó
-        let singersArray = Object.keys(data).map((key) => ({
+        const singersArray = Object.keys(data).map((key) => ({
           key: key,
           image: data[key].image,
           title: data[key].title,
